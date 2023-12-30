c81057fe52848eb89b76e402dd30603b17978cb5
Leonardo Kazu <kazuyoshikad@gmail.com>
2023-12-29 21:03:30 -0300

    criado DepartmentDaoJDBC / implementado insert e update

---
+++ src/application/Program2.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+    public static void main(String[] args) {
+        Department department = new Department(null, "Musica");
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+        //departmentDao.insert(department);
+    }
+}
